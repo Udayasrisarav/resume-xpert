442a443c8a447a8aa95c1a10b2ca07569348c742
udayasrisarav06 <udayasrisaravanan@gmail.com>
2026-06-20 19:32:50 +0530

    Deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter()],
+  base: process.env.VITE_BASE_PATH || "/resume-xpert",
   resolve: {
     tsconfigPaths: true,
   },
